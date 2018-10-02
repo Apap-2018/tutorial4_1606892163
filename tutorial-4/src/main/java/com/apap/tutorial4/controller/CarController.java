8c44faf69ccbd0579c1e6d75ec35048226ed8be5
Rico Putra Pradana <ricopradanaputra2198@gmail.com>
2018-10-02 14:44:48 +0700

    Implement controller

---
+++ tutorial-4/src/main/java/com/apap/tutorial4/controller/CarController.java
@@ -0,0 +1,38 @@
+package com.apap.tutorial4.controller;
+
+import com.apap.tutorial4.model.*;
+import com.apap.tutorial4.service.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * CarController
+ * @author rico.putra
+ * @version 2/10/18
+ */
+@Controller
+public class CarController {
+	@Autowired
+	private CarService carService;
+	
+	@Autowired
+	private DealerService dealerService;
+	
+	@RequestMapping(value = "/car/add/{dealerId}", method = RequestMethod.GET)
+	private String add(@PathVariable(value = "dealerId") Long dealerId, Model model) {
+		CarModel car = new CarModel();
+		DealerModel dealer = dealerService.getDealerDetailById(dealerId).get();
+		car.setDealer(dealer);
+		
+		model.addAttribute("car", car);
+		return "addCar";
+	}
+	
+	@RequestMapping(value = "/car/add", method = RequestMethod.POST)
+	private String addCarSubmit(@ModelAttribute CarModel car) {
+		carService.addCar(car);
+		return "add";
+	}
+}
---
+++ tutorial-4/src/main/java/com/apap/tutorial4/controller/DealerController.java
@@ -0,0 +1,40 @@
+package com.apap.tutorial4.controller;
+
+import java.util.List;
+import com.apap.tutorial4.model.*;
+import com.apap.tutorial4.service.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * DealerController
+ * @author rico.putra
+ * @version 2/10/18
+ */
+@Controller
+public class DealerController {
+	@Autowired
+	private DealerService dealerService;
+	
+	@Autowired
+	private CarService carService;
+	
+	@RequestMapping("/")
+	private String home() {
+		return "home";
+	}
+	
+	@RequestMapping(value = "/dealer/add", method = RequestMethod.GET)
+	private String add(Model model) {
+		model.addAttribute("dealer", new DealerModel());
+		return "addDealer";
+	}
+	
+	@RequestMapping(value = "/dealer/add", method = RequestMethod.POST)
+	private String addDealerSubmit(@ModelAttribute DealerModel dealer) {
+		dealerService.addDealer(dealer);
+		return "add";
+	}
+}
